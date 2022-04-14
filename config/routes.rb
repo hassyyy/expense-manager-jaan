399fe2c1b03d54537ffa57bb23cb4e98e8f506d8
hassyyy <nmohamedasan@gmail.com>
2022-04-14 23:49:58 +0530

    Add home dashboard as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "avo/home#index"
+  root "avo/dashboards#show", dashboard_id: 'home_dashboard'
 end
