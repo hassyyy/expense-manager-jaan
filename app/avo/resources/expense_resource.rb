52cbf21bd24e2b47e390428870a423bd839ad3a4
hassyyy <nmohamedasan@gmail.com>
2022-09-17 20:09:05 +0530

    Fix syntax error

--- app/avo/resources/expense_resource.rb
+++ app/avo/resources/expense_resource.rb
@@ -8,7 +8,7 @@ class ExpenseResource < Avo::BaseResource
   field :name, as: :text, link_to_resource: true, required: true
   field :amount, as: :number, required: true
   field :month, as: :select, options: AppOptions::MONTHS.hashify, default: -> { Date.today.strftime("%b") }
-  field :year, as: :number, min: AppOptions::MIN_YEAR, default: -> { Date.today.year
+  field :year, as: :number, min: AppOptions::MIN_YEAR, default: -> { Date.today.year }
   field :income, as: :boolean, default: false, hide_on: [:index]
 
   # filter MonthFilter
