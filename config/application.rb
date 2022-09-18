a9633eabb0d010ee81848c4c3e97721e372ce67d
hassyyy <nmohamedasan@gmail.com>
2022-09-18 21:48:14 +0530

    Add x-request-id to logs

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module ExpenseManager
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.log_tags = [ :request_id ]
   end
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,5 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
   config.logger = Logger.new(STDOUT)
 end
