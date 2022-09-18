cb83cfd41965ee78707fa314398bb191b2f56427
hassyyy <nmohamedasan@gmail.com>
2022-09-18 21:55:25 +0530

    Change logger

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Prepend all log lines with the following tags.
-  config.logger = Logger.new(STDOUT)
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
   config.log_tags = [ :request_id ]
 end
