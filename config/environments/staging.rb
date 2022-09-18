ec757b97ef6e206b460dbb01fcc4eadc4df3b114
hassyyy <nmohamedasan@gmail.com>
2022-09-18 21:50:49 +0530

    Add log tags after logger init

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,4 +3,5 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.logger = Logger.new(STDOUT)
+  config.log_tags = [ :request_id ]
 end
