23e9114bd6459c5ee246f578d9a5937d76e08e01
hassyyy <nmohamedasan@gmail.com>
2023-10-24 19:08:00 +0530

    Add currency symbol prefix

--- app/avo/cards/bank_balance_metric.rb
+++ app/avo/cards/bank_balance_metric.rb
@@ -3,13 +3,7 @@ include ActionView::Helpers::NumberHelper
 class BankBalanceMetric < Avo::Dashboards::MetricCard
   self.id = 'bank_balance_metric'
   self.label = 'Bank Balance'
-  # self.cols = 1
-  # self.rows = 1
-  # self.initial_range = 30
-  # self.ranges = [7, 30, 60, 365, 'TODAY', 'MTD', 'QTD', 'YTD', 'ALL']
-  # self.prefix = '$'
-  # self.suffix = '%'
-  # self.refresh_every = 10.minutes
+  self.prefix = '₹'
 
   query do
     income = Transaction.where(income: true, via: 'Bank').sum(:amount)
--- app/avo/cards/cash_balance_metric.rb
+++ app/avo/cards/cash_balance_metric.rb
@@ -1,13 +1,7 @@
 class CashBalanceMetric < Avo::Dashboards::MetricCard
   self.id = 'cash_balance_metric'
   self.label = 'Cash Balance'
-  # self.cols = 1
-  # self.rows = 1
-  # self.initial_range = 30
-  # self.ranges = [7, 30, 60, 365, 'TODAY', 'MTD', 'QTD', 'YTD', 'ALL']
-  # self.prefix = '$'
-  # self.suffix = '%'
-  # self.refresh_every = 10.minutes
+  self.prefix = '₹'
 
   query do
     income = Transaction.where(income: true, via: 'Cash').sum(:amount)
